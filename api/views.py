242ff65e48ec0356a7b5922bd2a73b3611a0f713
sampanacharya69@gmail.com <69064751+sampanacharya@users.noreply.github.com>
2022-10-17 10:24:23 +0530

    NOW I AM ACTUALLY DONE LOL

--- api/views.py
+++ api/views.py
@@ -32,16 +32,18 @@ class ImageViewSet(APIView):
 
 	def post(self, request):
 		
-		org = cv2.imdecode(np.fromstring(request.FILES['original'].read(), np.uint8), cv2.IMREAD_UNCHANGED)	
-		forg = cv2.imdecode(np.fromstring(request.FILES['forged'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
-		print(org.shape, forg.shape)
 		# original image
+		org = np.asarray(bytearray(request.FILES['original'].read()), dtype="uint8") 
+		org = cv2.imdecode(org, cv2.IMREAD_COLOR)
 		org = cv2.resize(org, (220, 155))
 		org = cv2.bitwise_not(org)
 		org = org / 255
 		org = np.expand_dims(org, axis=0)
 
 		# forged image
+		forg = np.asarray(bytearray(request.FILES['forged'].read()), dtype="uint8") 
+		forg = cv2.imdecode(forg, cv2.IMREAD_COLOR)
+		
 		forg = cv2.resize(forg, (220, 155))
 		forg = cv2.bitwise_not(forg)
 		forg = forg/255
@@ -53,3 +55,4 @@ class ImageViewSet(APIView):
 		assert(len(org.shape) == 4 and len(forg.shape) == 4),"Invalid Shape"
 		return Response({'score': mod.predict([org, forg])
 						})
+	
